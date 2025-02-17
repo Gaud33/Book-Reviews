e7d7ccb828289991e071c14d6b8d77fa98b88a00
Gaud33 <kafleegaurav@gmail.com>
2025-02-17 10:07:46 +0545

    Home route returnes trending, horror, fantasy books

--- server/routes/books.js
+++ server/routes/books.js
@@ -3,73 +3,84 @@ import axios from "axios";
 
 const router = express.Router();
 
-// default route
-router.get("/", async(req, res) =>{
-    try{
-        const response = await axios.get("https://openlibrary.org/search.json?title=harry+potter&author=rowling");
-        if(!response.data.docs.length){
-            return res.status(404).json({error:"No book found"});
-        }
-        const booksData = [];
-        //get the first response
-        const book = response.data.docs[0];
-        const books = {
-            title: book.title,
-            author: book.author_name[0],
-            coverURL: book.cover_i? `https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg`: "/images/book-cover-placeholder.jpeg"  
-        }
+// get book data from fetched response
+const getBookData = async (response) => {
+  if (!(response.data.docs || response.data.works)) {
+    throw new Error("Error getting data!");
+  }
+  // check for different api routes/ params
+  const bookData = response.data.docs
+    ? response.data.docs
+    : response.data.works;
 
-        booksData.push(books);
-        res.json(booksData);
-    }
+  return bookData.map((book) => ({
+    title: book.title,
+    author: book.author_name ? book.author_name[0] : "Unknown",
+    coverURL: book.cover_i
+      ? `https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg`
+      : "/images/book-cover-placeholder.jpeg",
+  }));
+};
 
-    
-    catch(error)
-    {
-        //handle error
-        console.error("Error fetching data: ", error.message);
-        res.status(500).json({error: "Internal Server Error"});
-    }
+// default home route
+router.get("/", async (req, res) => {
+  try {
+    // trending books
+    let response = await axios.get(
+      `https://openlibrary.org/trending/daily.json?limit=10`
+    );
+
+    const trendingBooks = await getBookData(response);
+
+    // horror books
+    response = await axios.get(
+      "https://openlibrary.org/subjects/horror.json?limit=10"
+    );
+    const horrorBooks = await getBookData(response);
+
+    // fantasy books
+    response = await axios.get(
+      "https://openlibrary.org/subjects/fantasy.json?limit=10"
+    );
+    const fantasyBooks = await getBookData(response);
+
+    res.json({
+      trending: trendingBooks,
+      horor: horrorBooks,
+      fantasy: fantasyBooks,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal Server Error!" });
+  }
 });
 
 // search route
-router.get("/search", async(req,res)=>{
-    const name = req.query.name;
-    if(!name){
-        return res.status(400).json({message: "Book name is required!"});
+router.get("/search", async (req, res) => {
+  const name = req.query.name;
+  if (!name) {
+    return res.status(400).json({ message: "Book name is required!" });
+  }
+
+  //encode spaces properly
+  const encodedName = encodeURIComponent(name);
+
+  try {
+    const response = await axios.get(
+      `https://openlibrary.org/search.json?title=${encodedName}`
+    );
+
+    if (!response.data.docs.length) {
+      return res.status(404).json({ error: "No book found" });
     }
+    const booksData = await getBookData(response);
 
-    //encode spaces properly
-    const encodedName = encodeURIComponent(name);
-
-    try{
-        const response = await axios.get(`https://openlibrary.org/search.json?title=${encodedName}`);
-
-        
-        if(!response.data.docs.length){
-            return res.status(404).json({error:"No book found"});
-        }
-
-        let booksData = [];
-        const bookList = response.data.docs;
-
-        booksData = bookList.map( book =>(
-            {
-            title: book.title,
-            author: book.author_name? book.author_name[0]: "Unknown", // for unspecified authors
-            coverURL: book.cover_i? `https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg`: "/images/book-cover-placeholder.jpeg"
-            }
-        ));
-   
-      res.json(booksData);
-
-    }
-
-    catch(error){
-        console.log(error);
-        res.status(500).json({error: "Internal Server Error!"});
-    }
-
-})
+    res.json(booksData);
+    
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal Server Error!" });
+  }
+});
 
 export default router;
